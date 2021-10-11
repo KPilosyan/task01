ca566f2bc7ff0237c475e8ea325c34aebfd9d15b
karen <pilosyan.karen@gmail.com>
2021-10-11 13:18:18 +0400

    Update product_routes.js

--- routes/product_routes.js
+++ routes/product_routes.js
@@ -9,47 +9,12 @@ router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
 
 router.get("/", auth(), productController.getProducts)
-router.get("/:id", productController.getSpecificProduct)
-router.post("/", productController.postProduct)
-router.put("/:id", productController.putProduct)
-router.delete("/:id", productController.deleteProduct)
+router.get("/:id", auth(), productController.getSpecificProduct)
+router.post("/", auth(), productController.postProduct)
+router.put("/:id", auth(), productController.putProduct)
+router.delete("/:id", auth(), productController.deleteProduct)
 
 
-// router.get((req, res) => Product.findAll()
-//         .then(prods => res.send(prods))
-//         .catch(err => console.log(err)));
-
-//   // Post
-// router.route("/")
-//     .post((req, res) => {
-//     let {name, color} = req.body
-
-//     Product.create({
-//         name, color})
-//         .then(prod => res.json())
-//         .catch(err => console.log(err))
-// });
-
-//   // Put
-// router.route("/:id")
-//     .put((req, res) => {
-//         let {name, color} = req.body 
-        
-//     Product.update(
-//         {name, color}, 
-//         {where: {id: req.params.id}} )
-//         .then(prod => res.redirect('/'))
-//         .catch(err => console.log(err))
-// });
-
-//  // Delete
-// router.route("/:id")
-//     .delete((req, res) => {
-//         Product.destroy(
-//             {where: {id: req.params.id}})
-//             .then(prod => res.redirect('/'))
-//             .catch(err => console.log(err))
-// });
 
 
 module.exports = router;
