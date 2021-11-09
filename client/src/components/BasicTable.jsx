4d0f40972327ed4ea2dc6a999a56d705e3d4eb21
karen <pilosyan.karen@gmail.com>
2021-11-09 19:44:21 +0400

    Table Fix
    Adding functionality of opening details of a particular row when it is clicked.

--- client/src/components/BasicTable.jsx
+++ client/src/components/BasicTable.jsx
@@ -1,88 +1,60 @@
-import { useMemo, useState } from 'react';
-import { useTable } from 'react-table';
+import { useState } from 'react';
 import './table.css';
 import '../App'
 
 
-const COLUMNS = [
-  {
-    Header: 'id',
-    accessor: 'id',
-  },
-  {
-    Header: 'name',
-    accessor: 'name',
-  },
-  {
-    Header: 'color',
-    accessor: 'color',
-  },
-];
-
-const RowDetails = (rowId) => {
+const RowDetails = () => {
   return (
-    <td key={rowId}>
+    <p>
       Some Details
-    </td>
+    </p>
+  )
+}
+
+export const TableRow = (props) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  const handleRowClick = () => {
+    showDetails ? setShowDetails(!showDetails) : setShowDetails(!showDetails)
+  }
+
+  return (
+    <>
+      <tr onClick={handleRowClick}>
+        <td>{props.product.id}</td>
+        <td>{props.product.name}</td>
+        <td>{props.product.color}</td>
+      </tr>
+      <tr>
+        {showDetails ? RowDetails() : null}
+      </tr>
+    </>
+
   )
 }
 
 const BasicTable = (props) => {
   const products = props.products
-  const [showDetails, setShowDetails] = useState(false)
-  const columns = useMemo(() => COLUMNS, []);
-
-  const handleShowDetails = (rowId) => {
-    if (showDetails) {
-      setShowDetails(false)
-      RowDetails(rowId)
-    }
-    else {
-      setShowDetails(!showDetails)
-    }
-  }
-
-  const tableInstance = useTable({
-    columns: columns,
-    data: products
-  });
-
-  const {
-    getTableProps, getTableBodyProps, headerGroups, rows, prepareRow,
-  } = tableInstance;
-
 
   return (
-
-    <table {...getTableProps()}>
-      <thead>
-        {headerGroups.map((headerGroup) => (
-          <tr key={headerGroup.id} {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map((column) => (
-              <th {...column.getHeaderProps()}>
-                {' '}
-                {column.render('Header')}
-                {' '}
-              </th>
-            ))}
-            <th />
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Color</th>
           </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map((row) => {
-          prepareRow(row);
-          return (
-            <>
-              <tr key={row.id} {...row.getRowProps()} onClick={handleShowDetails}>
-                {row.cells.map((cell) => <td {...cell.getCellProps()} >{cell.render('Cell')}  </td>)}
-              </tr>
-              {showDetails ? <RowDetails /> : null}
-            </>
-          );
-        })}
-      </tbody>
-    </table>
+        </thead>
+
+        <tbody id="products">
+          {
+            products.map(product => <TableRow key={product.id} product={product} />)
+          }
+        </tbody>
+      </table>
+
+    </>
 
   )
 };
